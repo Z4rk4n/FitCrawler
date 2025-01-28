b016fa0d8f1436d4a6c7b5dd401c1d9fb567ba57
miquel <alexandre.miquel.dev@gmail.com>
2025-01-28 23:23:39 +0100

    Now download gif - fill steps

--- main.php
+++ main.php
@@ -17,8 +17,10 @@ $crawler->filter('div.letter-section ul.az-columns li a')->each(function ($node)
     $exercices[] = [
         "name" => $node->text(),
         "link" => $node->attr("href"),
-        "gif-links" => [],
-        "description" => ""
+        "description" => "",
+        "steps" => [],
+        "advices" => [],
+        "gif-links" => []
     ];
 });
 
@@ -34,20 +36,38 @@ foreach ($exercices as $key => $exercice) {
         // downloadGif($node->attr("src"))
         $gifLink = $node->attr('src');
         if (preg_match("/(?<=\/)([a-zA-Z0-9-]+)\.gif$/", $gifLink, $matches)) {
+            downloadGif($gifLink, "assets/" . $matches[0]);
             $exercices[$key]["gif-links"]["link"] = $gifLink;
             $exercices[$key]["gif-links"]["name"] = $matches[0];
         }
     });
 
-    // textes
+    // descirption
     $description = "";
     $crawler->filter("main#main article.type-post div.post-wrap div.post-main p")->each(function ($node) use (&$description) {
         $description .= $node->text() . "\n\n";
     });
     $exercices[$key]["description"] = trim(str_replace("Ne manquez aucun article ou étude publiés ! Suivre nos articles sur Google News", "", $description));
 
-    print_r($exercices[$key]);
+    // steps
+    $steps = [];
+
+    $crawler->filter("div.post-main ol.wp-block-list li")->each(function ($node) use (&$steps) {
+        $steps[] = trim($node->text());
+    });
+
+    $exercices[$key]["steps"] = $steps;
+
+    // advices
+    $advices = [];
+    $crawler->filter("div.post-main ul.wp-block-list li")->each(function ($node) use (&$advices) {
+        $advices[] = trim($node->text());
+    });
+
+    $exercices[$key]["advices"] = $advices;
 }
+
+file_put_contents("exercices.json", json_encode($exercices, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 
 
 
