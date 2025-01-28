d11bfeb87c1161cccf7946c858283446a8033cbc
miquel <alexandre.miquel.dev@gmail.com>
2025-01-28 22:13:43 +0100

    Parse gif links & get description for exercice

--- functions.php
+++ functions.php
@@ -21,3 +21,24 @@ function getHtmlFromUrl($url) {
     
     return $html;
 }
+
+function downloadGif($url, $destination) {
+    // Initialiser cURL
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Retourner le contenu du fichier
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Suivre les redirections
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true); // Vérifier le certificat SSL
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);    // Vérifier l'hôte SSL
+
+    // Exécuter la requête
+    $data = curl_exec($ch);
+
+    // Vérifier les erreurs cURL
+    if (curl_errno($ch)) {
+        die("Erreur lors du téléchargement : " . curl_error($ch));
+    }
+
+    curl_close($ch);
+
+    file_put_contents($destination, $data);
+}
--- main.php
+++ main.php
@@ -16,15 +16,37 @@ $crawler = $client->request('GET', $url);
 $crawler->filter('div.letter-section ul.az-columns li a')->each(function ($node) use (&$exercices) {
     $exercices[] = [
         "name" => $node->text(),
-        "url" => $node->attr("href")
+        "link" => $node->attr("href"),
+        "gif-links" => [],
+        "description" => ""
     ];
 });
 
-
-foreach ($exercices as $exercice) {
-    sleep(1000);
-    $exerciceName = $exercice["name"];
+foreach ($exercices as $key => $exercice) {
+    sleep(0.5);
     echo " -- Parsing Exercice: " . $exercice["name"] . " -- \n";
+
+    // load page
+    $crawler = $client->request('GET', $exercice["link"]);
+    
+    // gif
+    $crawler->filter("div.post-main section.entry-content div.wp-block-image figure img")->each(function ($node) use (&$exercices, $key) {
+        // downloadGif($node->attr("src"))
+        $gifLink = $node->attr('src');
+        if (preg_match("/(?<=\/)([a-zA-Z0-9-]+)\.gif$/", $gifLink, $matches)) {
+            $exercices[$key]["gif-links"]["link"] = $gifLink;
+            $exercices[$key]["gif-links"]["name"] = $matches[0];
+        }
+    });
+
+    // textes
+    $description = "";
+    $crawler->filter("main#main article.type-post div.post-wrap div.post-main p")->each(function ($node) use (&$description) {
+        $description .= $node->text() . "\n\n";
+    });
+    $exercices[$key]["description"] = trim(str_replace("Ne manquez aucun article ou étude publiés ! Suivre nos articles sur Google News", "", $description));
+
+    print_r($exercices[$key]);
 }
 
 
